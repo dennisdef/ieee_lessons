a3a105800b9bdaab2eb73d60bcb41faa90b03c9a
dennisdef <dennis.deftereos@gmail.com>
2024-12-03 16:44:45 +0200

    Lesson 4

---
+++ Lesson 4/microblog/app/routes.py
@@ -0,0 +1,57 @@
+from app import app, db
+from app.forms import LoginForm
+from app.models import User
+
+from flask import flash, render_template, redirect, request, url_for
+import sqlalchemy as sa
+from flask_login import current_user, login_required, login_user, logout_user
+
+from urllib.parse import urlsplit
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    users = {'username': 'Mixalis'}	
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=users, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if (user == None) or (not user.check_password(form.password.data)):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+    
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    return 'User %s' % username
