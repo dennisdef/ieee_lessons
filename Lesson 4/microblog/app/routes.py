b84948ad72038741cf0ceb16d18ec02b478b573a
dennisdef <dennis.deftereos@gmail.com>
2024-12-03 17:33:16 +0200

    Lesson 4

--- Lesson 4/microblog/app/routes.py
+++ Lesson 4/microblog/app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, SignupForm
 from app.models import User
 
 from flask import flash, render_template, redirect, request, url_for
@@ -12,7 +12,6 @@ from urllib.parse import urlsplit
 @app.route('/index')
 @login_required
 def index():
-    users = {'username': 'Mixalis'}	
 
     posts = [
         {
@@ -25,7 +24,7 @@ def index():
         }
     ]
 
-    return render_template('index.html', title='Home', user=users, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -50,6 +49,20 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Register', form=form)
 
 @app.route('/user/<username>')
 @login_required
