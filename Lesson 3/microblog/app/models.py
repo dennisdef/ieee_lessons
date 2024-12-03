2fb69efa7026dbb59e4dd789a223203e0141ece9
dennisdef <dennis.deftereos@gmail.com>
2024-12-03 15:58:41 +0200

    Lesson 3

---
+++ Lesson 3/microblog/app/models.py
@@ -0,0 +1,27 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from typing import Optional
+from app import db
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(128))
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+
+    def __repr__(self):
+        return '<User {} {}>'.format(self.username, self.email)
+    
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[Optional[str]] = so.mapped_column(sa.DateTime)
+
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
---
+++ Lesson 3/microblog/app/routes.py
@@ -0,0 +1,37 @@
+from app import app
+from app.forms import LoginForm
+
+from flask import flash, render_template, redirect
+
+
+@app.route('/')
+def index():
+    users = {'username': 'Mixalis'}	
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=users, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print('Login requested for user {}, password {}'.format(form.username.data, form.password.data))
+        flash('Login requested for user {}, password {}'.format(form.username.data, form.password.data))
+        return redirect('/')
+
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/user/<username>')
+def user(username):
+    return 'User %s' % username
